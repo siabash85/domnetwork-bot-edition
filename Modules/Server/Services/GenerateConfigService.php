9771de5d83fa10fc84b03c89f8665706182550c1
Hilson <hilsonxhero.me@gmail.com>
2024-01-28 02:04:24 +0330

    Update GenerateConfigService.php

--- Modules/Server/Services/GenerateConfigService.php
+++ Modules/Server/Services/GenerateConfigService.php
@@ -103,8 +103,8 @@ class GenerateConfigService
 
     public static function generateConfigQrCode($link)
     {
-        $qrCode = QrCode::format('png')->generate($link);
-        $path = 'public/images/qrcodes/' . uniqid() . '.png';
+        $qrCode = QrCode::format('jpg')->generate($link);
+        $path = 'public/images/qrcodes/' . uniqid() . '.jpg';
         Storage::put($path, $qrCode);
         $qrcode = Storage::url($path);
         return $qrcode;
